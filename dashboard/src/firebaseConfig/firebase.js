39df34e2c19e9ed69c68598c31a301b533516bb4
rodriguezemautn <erodriguezrodriguez@alu.frlp.utn.edu.ar>
2022-09-26 21:31:44 -0300

    ADD configuracion firebase export db

---
+++ dashboard/src/firebaseConfig/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6bvsBEjWfSZbUKPw7rFpt5MY2NOsRmQ8",
+  authDomain: "g2agiles2022.firebaseapp.com",
+  projectId: "g2agiles2022",
+  storageBucket: "g2agiles2022.appspot.com",
+  messagingSenderId: "804659025270",
+  appId: "1:804659025270:web:024550fa5ca36a4e025657"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
